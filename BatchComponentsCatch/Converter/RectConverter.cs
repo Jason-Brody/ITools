e1626067612e271f9d295db579944b702a4f9501
kav7860 <kav7860@gmail.com>
2015-08-30 21:38:35 +0800

    BF

---
+++ BatchComponentsCatch/Converter/RectConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace BatchComponentsCatch.Converters
+{
+    public class RectConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double width = double.Parse(values[0].ToString());
+            double height = double.Parse(values[1].ToString());
+            return new Rect(0, 0, width, height);
+        }
+
+       
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
--- BatchComponentsCatch/MainWindow.xaml.cs
+++ BatchComponentsCatch/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SAPAutomation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SAPGuiAutomationLib;
+using SAPFEWSELib;
 
 namespace BatchComponentsCatch
 {
@@ -21,16 +24,32 @@ namespace BatchComponentsCatch
     public partial class MainWindow : Window
     {
         Point start;
-        Point temp;
+        Point end;
         bool isStartSet;
+        IEnumerable<GuiVComponent> selectedComponents;
         public MainWindow()
         {
             InitializeComponent();
+            SAPTestHelper.Current.SetSession();
+            SAPTestHelper.Current.SAPGuiSession.ActiveWindow.Restore();
+            this.Width = SAPTestHelper.Current.SAPGuiSession.ActiveWindow.Width;
+            
+            this.Height = SAPTestHelper.Current.SAPGuiSession.ActiveWindow.Height;
+
+            this.Left = SAPTestHelper.Current.SAPGuiSession.ActiveWindow.Left;
+            this.Top = SAPTestHelper.Current.SAPGuiSession.ActiveWindow.Top;
         }
 
         private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            start = e.GetPosition(cv);
+            if(selectedComponents!=null)
+            {
+                foreach(var comp in selectedComponents)
+                {
+                    comp.Visualize(false);
+                }
+            }
+            start = e.GetPosition(this);
             rect.SetValue(Canvas.LeftProperty, start.X);
             rect.SetValue(Canvas.TopProperty, start.Y);
             isStartSet = true;
@@ -38,22 +57,61 @@ namespace BatchComponentsCatch
 
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
-            if(isStartSet)
+            if(isStartSet && e.LeftButton == MouseButtonState.Pressed)
             {
-                temp = e.GetPosition(cv);
-                if (temp.X < start.X)
+                end = e.GetPosition(this);
+                
+                if (end.X < start.X)
                 {
-                    rect.SetValue(Canvas.LeftProperty, temp.X);
+                    rect.SetValue(Canvas.LeftProperty, end.X);
                 }
-                if (temp.Y < start.Y)
+                if (end.Y < start.Y)
                 {
-                    rect.SetValue(Canvas.TopProperty, temp.Y);
+                    rect.SetValue(Canvas.TopProperty, end.Y);
                 }
-                rect.Width = Math.Abs(temp.X - start.X);
-                rect.Height = Math.Abs(temp.Y - start.Y);
+                double width = Math.Abs(end.X - start.X);
+                double height = Math.Abs(end.Y - start.Y);
+             
+                
+                rect.Width = width;
+                rect.Height = height;
+                rectG_Area.Rect = new Rect(start,end);
+
+                X1.Text = start.X.ToString();
+                X2.Text = end.X.ToString();
+                Y1.Text = start.Y.ToString();
+                Y2.Text = end.Y.ToString();
+
             }
             
             
+        }
+
+        private void cv_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            int X1, X2, Y1, Y2;
+            X1 = (int)start.X;
+            X2 = (int)end.X;
+            Y1 = (int)start.Y;
+            Y2 = (int)end.Y;
+            if(end.X < start.X)
+            {
+                X1 = (int)end.X;
+                X2 = (int)start.X;
+            }
+            if(end.Y < start.Y)
+            {
+                Y1 = (int)end.Y;
+                Y2 = (int)start.Y;
+            }
+
+
+            selectedComponents = SAPTestHelper.Current.SAPGuiSession.ActiveWindow.FindByRegion(X1,Y1,X2,Y2);
+            foreach (var c in selectedComponents)
+            {
+                c.Visualize(true);
+            }
+            tb.Text = selectedComponents.Count().ToString();
         }
     }
 }
